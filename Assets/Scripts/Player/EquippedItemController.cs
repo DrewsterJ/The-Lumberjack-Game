1437db54a39f1032e775590b02626b84461a9c54
DrewsterJ <drewpjonester@gmail.com>
2024-02-11 18:01:26 -0500

    Improved user equipment controls

--- Assets/Scripts/Player/EquippedItemController.cs
+++ Assets/Scripts/Player/EquippedItemController.cs
@@ -8,16 +8,23 @@ public class EquippedItemController : MonoBehaviour
 {
     public GameObject activeItem;
     public Animator activeItemClickAnim;
+    private float lastActionStartTime;
 
     private static readonly int Clicked = Animator.StringToHash("Clicked");
 
     private void Start()
     {
+        lastActionStartTime = Time.time;
         Debug.Assert(!activeItem.IsUnityNull());
     }
 
     public void PerformLeftClickAnimation()
     {
+        if (Math.Abs(lastActionStartTime - Time.time) < 0.5f)
+            return;
+
+        lastActionStartTime = Time.time;
+        
         activeItemClickAnim.SetTrigger(Clicked);
     }
 }
